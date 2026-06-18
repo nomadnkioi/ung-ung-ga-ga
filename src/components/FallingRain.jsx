40466ff1e29b850a0e625a2c0633ad2ebd8bc9fc
nomadnkioi <nomadnkioi@gmail.com>
2026-06-18 20:11:30 +0900

    style: remove '응응', '가가' text from splash screen

--- src/components/FallingRain.jsx
+++ src/components/FallingRain.jsx
@@ -5,15 +5,7 @@ const FallingRain = ({ onEnter }) => {
   return (
     <div className="landing-screen" onClick={onEnter}>
       <div className="landing-content">
-        <h1 className="landing-title-split top">
-          <span className="char" style={{ backgroundColor: '#ff80ab' }}>응</span>
-          <span className="char" style={{ backgroundColor: '#80d8ff' }}>응</span>
-        </h1>
         <img src={rainbowPoop} alt="Rainbow Poop" className="hero-poop" />
-        <h1 className="landing-title-split bottom">
-          <span className="char" style={{ backgroundColor: '#69f0ae' }}>가</span>
-          <span className="char" style={{ backgroundColor: '#ffd180' }}>가</span>
-        </h1>
       </div>
     </div>
   );
